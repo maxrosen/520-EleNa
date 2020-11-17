0dc264b1af41e48d27d00b00010d54ec33be7375
Max Rosen <mfrosen100@gmail.com>
2020-11-17 14:06:37 -0500

    implemented maximization algorithm, added corresponding tests

--- src/model/model.py
+++ src/model/model.py
@@ -10,33 +10,63 @@ class Model(object):
     def set_view(self, vobj):
         self.vobj = vobj
 
-    def get_route(self, G, origin, destination, extra_travel, mode='minimize'):
-        shortest_path = nx.shortest_path(
-            G, origin, destination, weight='length')
+    def get_route(self, G, origin, destination, extra_travel, mode):
+        shortest_path = nx.shortest_path(G, origin, destination, weight='length')
         print()
         print("Printing Statistics of Shortest path route")
         self.print_route_stats(G, shortest_path)
-
-        shortest_elevation_path = nx.shortest_path(
-            G, origin, destination, weight='elevation')
-        print()
-        print("Printing Statistics of Shortest Elevation path route")
-        self.print_route_stats(G, shortest_elevation_path)
 
         shortest_path_length = self.get_total_length(G, shortest_path)
         can_travel = ((100.0 + extra_travel)*shortest_path_length)/100.0
         print()
         print("Distance you are willing to travel : ", can_travel)
 
-        # Strategy1
         t = time.time()
-        route_minimize_elevation1 = self.dijkstra_search(G, origin, destination, can_travel, mode)
+        optimized_route = self.get_op_route(G, origin, destination, extra_travel, mode)
         print()
-        print("Dijkstra algorithm took :", time.time()-t, " seconds")
-        print("Printing Statistics of our algorithm's minimum Elevation route")
-        self.print_route_stats(G, route_minimize_elevation1)
-        self.vobj.show_route(G, route_minimize_elevation1, alt_route=[
-                             shortest_path, shortest_elevation_path])
+        print("Our algorithm took :", time.time() - t, " seconds")
+        print("Printing Statistics of our algorithm's " + mode + "d elevation route")
+        self.print_route_stats(G, optimized_route)
+        self.vobj.show_route(G, optimized_route, alt_route=[shortest_path])
+
+    def get_op_route(self, G, start, end, extra_travel, mode):
+        if(mode == 'maximize'):
+            return self.max_ele(G, start, end, extra_travel)
+        else:
+            return self.min_ele(G, start, end, extra_travel)
+
+    def max_ele(self, G, source, goal, extra_travel):
+        shortest_path = nx.shortest_path(G, source, goal, weight='length')
+        shortest_path_length = self.get_total_length(G, shortest_path)
+        max_path_length = shortest_path_length * (1 + (extra_travel/100))
+        max_path = []
+        length_allowance = max_path_length - shortest_path_length
+        if (extra_travel < 0.05):
+            return shortest_path
+
+        for i in range(0, len(shortest_path) - 1):
+            cur_node = shortest_path[i]
+            next_node = shortest_path[i + 1]
+            min_distance = G[cur_node][next_node][0]['length']
+            allowance = length_allowance * (min_distance / shortest_path_length)
+            highest_elevation = -1
+            best_path = []
+
+            for path in nx.all_simple_paths(G, cur_node, next_node, cutoff=5):
+                path_elevation = self.get_elevation_stats(G, path)["ascents"]
+                path_length = self.get_total_length(G, path)
+                if path_elevation > highest_elevation:
+                    if path_length <= allowance + min_distance:
+                        highest_elevation = path_elevation
+                        best_path = path
+
+            best_path_length = self.get_total_length(G, best_path)
+            length_allowance -= (best_path_length - min_distance)
+
+            for j in best_path[:-1]:
+                max_path.append(j)
+        max_path.append(goal)
+        return max_path
 
     def get_elevation_cost(self, G, a, b):
         return (G.nodes[a]['elevation'] - G.nodes[b]['elevation'])
@@ -86,7 +116,7 @@ class Model(object):
             cost += self.get_cost(G, route[i], route[i+1])
         return cost
 
-    def dijkstra_search(self, G, start, goal, viablecost, mode='minimize'):
+    def min_ele(self, G, start, goal, viablecost):
         frontier = []
         heappush(frontier, (0, start))
         came_from = {}
@@ -107,12 +137,10 @@ class Model(object):
                 elevationcost = self.get_elevation_cost(G, current, next)
                 if elevationcost > 0:
                     new_cost_ele = new_cost_ele + elevationcost
-                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                if next not in cost_so_far_ele or new_cost_ele < cost_so_far_ele[next]:
                     cost_so_far_ele[next] = new_cost_ele
                     cost_so_far[next] = new_cost
                     priority = new_cost_ele
-                    if mode == 'maximize':
-                        priority = priority
                     heappush(frontier, (priority, next))
                     came_from[next] = current
         return self.get_path(came_from, start, goal)

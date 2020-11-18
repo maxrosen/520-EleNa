e3c6c44ac0a94e065ce22e7f5c85128277e8b319
kdstevens <kdstevens@umass.edu>
2020-11-17 21:33:49 -0500

    Added an improved max elevation search algorithm

--- src/model/model.py
+++ src/model/model.py
@@ -31,6 +31,8 @@ class Model(object):
 
 
     def get_op_route(self, G, start, end, can_travel, mode):
+        # Uncomment the line below to use the improved max elevation search. I'll make this pretty later 
+        # self.max_ele = self.get_max_ele_hill_climb
         if(mode == 'maximize'):
             return self.max_ele(G, start, end, can_travel)
         else:
@@ -99,11 +101,67 @@ class Model(object):
                     came_from[next] = current
         return self.get_path(came_from, start, goal)
 
+    def get_max_ele_hill_climb(self, G, start, goal, can_travel):
+        
+        # Get the shortest path and its length 
+        shortest_path = nx.shortest_path(G, start, goal, weight='length')
+        shortest_path_length = self.get_total_length(G, shortest_path)
+        
+        if (can_travel == shortest_path_length):
+            return shortest_path
+
+        curr_path = []
+        bad_nodes = []
+        curr_node = start
+        curr_path.append(curr_node)
+        
+        while curr_node != goal:
+            # Get the neighbors of the current node
+            neighbors = G.neighbors(curr_node)
+
+            # Filter the neighbors to find the valid possible next nodes
+            valid_neighbors = []
+            for nbr in neighbors:
+                # Filter out the neighbors that are in the path and have no valid neighbors
+                if (nbr in curr_path) or (nbr in bad_nodes):
+                    continue
+
+                nbr_goal_path = nx.shortest_path(G, nbr, goal, weight='length')
+                nbr_goal_length = self.get_total_length(G, nbr_goal_path)
+                curr_path_length = self.get_total_length(G, curr_path)
+                curr_nbr_edge_cost = self.get_cost(G, curr_node, nbr)
+
+                # Append only the nodes that will not cause the path length to be too long
+                if curr_path_length + curr_nbr_edge_cost + nbr_goal_length <= can_travel:
+                    valid_neighbors.append(nbr)
+
+            # If a node has no valid neighbors
+            if (len(valid_neighbors) == 0):
+                # Mark the node as bad
+                bad_nodes.append(curr_node)
+                # Backtrack on the path
+                curr_path = curr_path[:-1]
+                # Set the current node to the end of the path (previous node)
+                curr_node = curr_path[-1]
+            
+            # If a node does have valid neighbors
+            else:
+                # Find the valid neighbor with the highest elevation, add it to the path, and update the current node 
+                max_ele_vnbr = valid_neighbors[0]
+                for vnbr in valid_neighbors:
+                    if G.nodes[vnbr]["elevation"] > G.nodes[max_ele_vnbr]["elevation"]:
+                        max_ele_vnbr = vnbr
+                curr_path.append(max_ele_vnbr)
+                curr_node = max_ele_vnbr
+
+        return curr_path
+
     def get_elevation_cost(self, G, a, b):
         return (G.nodes[a]['elevation'] - G.nodes[b]['elevation'])
 
     def get_cost(self, G, a, b):
-        return G.edges[a, b, 0]['length']
+        edge = G.edges[a, b, 0]
+        return edge['length']
 
     def get_path(self, came_from, origin, destination):
         route_by_length_minele = []
@@ -140,7 +198,7 @@ class Model(object):
         return elevation_stats
 
     def get_total_length(self, G, route):
-        if route is None:
+        if route is None or len(route) <= 1:
             return 0
         cost = 0
         for i in range(len(route)-1):
